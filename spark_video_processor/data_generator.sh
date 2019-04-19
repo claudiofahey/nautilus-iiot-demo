d4e430135a749e7c39f38c0711f72adbe1a2d3b0
Claudio Fahey <claudio.fahey@dell.com>
2019-04-19 14:02:44 -0700

    Changed chunking algorithm

--- spark_video_processor/data_generator.sh
+++ spark_video_processor/data_generator.sh
@@ -2,5 +2,5 @@
 set -e
 export PATH=$PWD/env/bin:$PATH
 export PYTHONPATH=$PWD/../pravega-gateway/src/main/python
-export GENERATOR_SCOPE=${PRAVEGA_SCOPE:-examples11}
+export GENERATOR_SCOPE=${PRAVEGA_SCOPE:-examples13}
 src/main/python/data_generator.py $*
