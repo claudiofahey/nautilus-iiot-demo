966778f03c500d37d5cc4a002a0290c5bcd497bc
Claudio Fahey <claudio.fahey@dell.com>
2019-04-27 23:46:20 -0700

    Added Jupyter notebook to write PNGs from Pravega

--- scripts/deploy-k8s-components.sh
+++ scripts/deploy-k8s-components.sh
@@ -5,17 +5,18 @@ set -ex
 : ${IMAGE_TAG?"You must export IMAGE_TAG"}
 
 ROOT_DIR=$(dirname $0)/..
+NAMESPACE=examples
 
-helm install \
-    --name pravega-gateway \
-    --namespace examples \
-    ${ROOT_DIR}/charts/pravega-gateway \
-    --set image.repository=${DOCKER_REPOSITORY}/pravega-gateway \
-    --set image.tag=${IMAGE_TAG}
+helm upgrade --install \
+pravega-gateway \
+--namespace ${NAMESPACE} \
+${ROOT_DIR}/charts/pravega-gateway \
+--set image.repository=${DOCKER_REPOSITORY}/pravega-gateway \
+--set image.tag=${IMAGE_TAG}
 
-helm install \
-    --name streaming-data-generator \
-    --namespace examples \
-    ${ROOT_DIR}/charts/streaming_data_generator \
-    --set image.repository=${DOCKER_REPOSITORY}/streaming_data_generator \
-    --set image.tag=${IMAGE_TAG}
+helm upgrade --install \
+streaming-data-generator \
+--namespace ${NAMESPACE} \
+${ROOT_DIR}/charts/streaming_data_generator \
+--set image.repository=${DOCKER_REPOSITORY}/streaming_data_generator \
+--set image.tag=${IMAGE_TAG}
