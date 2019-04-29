194a45d33a44983501c96c1e468b1dd10affba1c
Claudio Fahey <claudio.fahey@dell.com>
2019-04-28 21:32:03 -0700

    Updated to JupyterHub to Spark 2.4.1

--- jupyterhub/build.sh
+++ jupyterhub/build.sh
@@ -4,7 +4,9 @@ set -ex
 : ${DOCKER_REPOSITORY?"You must export DOCKER_REPOSITORY"}
 
 SOURCE_IMAGE=jupyter/all-spark-notebook
-SOURCE_TAG=2343e33dec46
+#SOURCE_TAG=2343e33dec46     # 2.4.0
+SOURCE_TAG=f646d2b2a3af      # 2.4.1
+#SOURCE_TAG=ae5f7e104dd5     # 2.4.2
 TARGET_IMAGE=${DOCKER_REPOSITORY}/all-spark-notebook
 TARGET_TAG=${TARGET_TAG:-${SOURCE_TAG}}
 
